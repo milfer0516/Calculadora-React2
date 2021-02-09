18244b57c8a8a79ac2f15692eb83823a2841f8d9
milfer0516 <milfer0516@gmail.com>
2021-02-09 10:58:20 -0500

    adaptar responsive

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -45,70 +45,81 @@ const Calculadora = () => {
 
     return (
         <div className="App">
-            <h1>Calculadora</h1>
+            <div className="container">
+                <h1>Calculadora</h1>
 
-            <div className="calc-app">
-                <form>
-                    <input type="text" value={resultado} ref={inputRef}></input>
-                </form>
-                <div className="botones">
-                    <button name="" id="reset" onClick={reset}>
-                        C
-                    </button>
-                    <button onClick={eventOnclick} id="dividir" name="/">
-                        /
-                    </button>
-                    <button onClick={eventOnclick} id="multiplicar" name="*">
-                        *
-                    </button>
-                    <button id="clear" id="clear" onClick={clear}>
-                        x
-                    </button>
-                    <button onClick={eventOnclick} name="7">
-                        7
-                    </button>
-                    <button onClick={eventOnclick} name="8">
-                        8
-                    </button>
-                    <button onClick={eventOnclick} name="9">
-                        9
-                    </button>
-                    <button onClick={eventOnclick} id="restar" name="-">
-                        -
-                    </button>
-                    <button onClick={eventOnclick} name="4">
-                        4
-                    </button>
-                    <button onClick={eventOnclick} name="5">
-                        5
-                    </button>
-                    <button onClick={eventOnclick} name="6">
-                        6
-                    </button>
-                    <button onClick={eventOnclick} id="sumar" name="+">
-                        +
-                    </button>
-                    <button onClick={eventOnclick} name="1">
-                        1
-                    </button>
-                    <button onClick={eventOnclick} name="2">
-                        2
-                    </button>
-                    <button onClick={eventOnclick} name="3">
-                        3
-                    </button>
-                    <button id="result" onClick={result} name="=">
-                        =
-                    </button>
-                    <button onClick={eventOnclick} name="%">
-                        %
-                    </button>
-                    <button onClick={eventOnclick} name="0">
-                        0
-                    </button>
-                    <button onClick={eventOnclick} name=".">
-                        .
-                    </button>
+                <div className="calc-app">
+                    <form>
+                        <input
+                            type="text"
+                            value={resultado}
+                            ref={inputRef}
+                        ></input>
+                    </form>
+                    <hr />
+                    <div className="botones">
+                        <button name="" id="reset" onClick={reset}>
+                            C
+                        </button>
+                        <button onClick={eventOnclick} id="dividir" name="/">
+                            /
+                        </button>
+                        <button
+                            onClick={eventOnclick}
+                            id="multiplicar"
+                            name="*"
+                        >
+                            *
+                        </button>
+                        <button id="clear" id="clear" onClick={clear}>
+                            x
+                        </button>
+                        <button onClick={eventOnclick} name="7">
+                            7
+                        </button>
+                        <button onClick={eventOnclick} name="8">
+                            8
+                        </button>
+                        <button onClick={eventOnclick} name="9">
+                            9
+                        </button>
+                        <button onClick={eventOnclick} id="restar" name="-">
+                            -
+                        </button>
+                        <button onClick={eventOnclick} name="4">
+                            4
+                        </button>
+                        <button onClick={eventOnclick} name="5">
+                            5
+                        </button>
+                        <button onClick={eventOnclick} name="6">
+                            6
+                        </button>
+                        <button onClick={eventOnclick} id="sumar" name="+">
+                            +
+                        </button>
+                        <button onClick={eventOnclick} name="1">
+                            1
+                        </button>
+                        <button onClick={eventOnclick} name="2">
+                            2
+                        </button>
+                        <button onClick={eventOnclick} name="3">
+                            3
+                        </button>
+                        <button id="result" onClick={result} name="=">
+                            =
+                        </button>
+                        <button onClick={eventOnclick} name="%">
+                            %
+                        </button>
+                        <button onClick={eventOnclick} name="0">
+                            0
+                        </button>
+                        <button onClick={eventOnclick} name=".">
+                            .
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
